202efce1f06b4a2bed337c282c2168a860665bcd
David Wiesen <iceweasel@gmail.com>
2015-03-21 19:48:50 -0700

    jokes app

---
+++ jokes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth import models as auth_models
+
+
+class Joke(models.Model):
+	title = models.CharField(max_length=1024)
+    description = models.CharField()
+    date_entered = models.DateTimeField('date entered')
+    entered_by = models.ForeignKey(auth_models.User)
