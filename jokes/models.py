c50b2ae8bfccd50be1bcb588d531fed61cd7342f
David Wiesen <david@nextdoor.com>
2015-11-30 09:09:54 -0800

    fix indentation error in jokes models

--- jokes/models.py
+++ jokes/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import models as auth_models
 
 
 class Joke(models.Model):
-	title = models.CharField(max_length=1024)
+    title = models.CharField(max_length=1024)
     description = models.CharField()
     date_entered = models.DateTimeField('date entered')
     entered_by = models.ForeignKey(auth_models.User)
